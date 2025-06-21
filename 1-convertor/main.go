ba71953eeda0e6962b2c651168b7653656c8823b
Maksim G <magus.mgl@gmail.com>
2025-06-21 22:09:34 +0500

    fix homework

--- 1-convertor/main.go
+++ 1-convertor/main.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	const usdToEur float64 = 0.87
 	const usdToRub float64 = 78.38
-	eurToRub := usdToEur * usdToRub
+	eurToRub := usdToRub / usdToEur
 	fmt.Print("Курс EUR в  RUB  - ", eurToRub)
 }
