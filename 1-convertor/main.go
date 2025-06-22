eb7fcfa6ad1962c06fb43f6e9bd99fe0ae4d9a28
Maksim G <magus.mgl@gmail.com>
2025-06-22 15:43:07 +0500

    homework2

--- 1-convertor/main.go
+++ 1-convertor/main.go
@@ -5,6 +5,24 @@ import "fmt"
 func main() {
 	const usdToEur float64 = 0.87
 	const usdToRub float64 = 78.38
-	eurToRub := usdToRub / usdToEur
-	fmt.Print("Курс EUR в  RUB  - ", eurToRub)
+	// eurToRub := usdToRub / usdToEur
+
+	originalCurrency, targetCurrency, amountCurrency := getUserInput()
+	convertCurrency(originalCurrency, targetCurrency, amountCurrency)
+}
+
+func getUserInput() (string, string, float64) {
+	var originalCurrency string
+	var targetCurrency string
+	var amountCurrency float64
+	fmt.Print("Введите исходную валюту: ")
+	fmt.Scan(&originalCurrency)
+	fmt.Print("Введите целевую валюту: ")
+	fmt.Scan(&targetCurrency)
+	fmt.Print("Введите количество валюты для конвертации")
+	fmt.Scan(&amountCurrency)
+	return originalCurrency, targetCurrency, amountCurrency
+}
+
+func convertCurrency(originalCurrency string, targetCurrency string, amountCurrency float64) {
 }
