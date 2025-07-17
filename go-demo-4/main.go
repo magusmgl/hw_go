b6949cdac8872b91a0ca121418bd4278ecd2f7db
Maksim G <magus.mgl@gmail.com>
2025-07-18 00:06:50 +0500

    структуры

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -1,17 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
+func (acc *account) outputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(int(len(letterRunes)))]
+	}
+	acc.password = string(res)
+}
+
+func newAccount(login string, password string, url string) *account {
+	return &account{
+		login:    login,
+		password: password,
+		url:      url,
+	}
+}
+
+var letterRunes = []rune("abcdefghijklmnoprstuwvyzABCDEFJHOQPRSTYUXYWZ1234567890-*!")
 
 func main() {
-	a := 5
-	fmt.Println(&a)
-	double(&a)
-	fmt.Println(a)
+	// a := 5
+	// fmt.Println(&a)
+	// double(&a)
+	// fmt.Println(a)
 
-	b := [4]int{1, 2, 3, 4}
-	reverse(&b)
-	fmt.Println(b)
+	// b := [4]int{1, 2, 3, 4}
+	// reverse(&b)
+	// fmt.Println(b)
+	str := []rune("Привет)")
+	for _, ch := range string(str) {
+		fmt.Println(ch, string(ch))
+	}
 
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
+	url := promptData("Введите URL")
+
+	myAccount := newAccount(login, password, url)
+	myAccount.generatePassword(12)
+	myAccount.outputPassword()
+
+}
+
+func promptData(prompt string) string {
+	fmt.Print(prompt + ": ")
+	var res string
+	fmt.Scan(&res)
+	return res
 }
 
 func double(num *int) {
