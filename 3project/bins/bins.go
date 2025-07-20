5e4b85eaf4f471a47a08d6ff3133c27223f0e572
Maksim G <magus.mgl@gmail.com>
2025-07-20 21:46:41 +0500

    homework packages

---
+++ 3project/bins/bins.go
@@ -0,0 +1,42 @@
+package bins
+
+import (
+	"errors"
+	"strings"
+	"time"
+)
+
+type Bin struct {
+	Id        string
+	Private   bool
+	CreatedAt time.Time
+	Name      string
+}
+
+func NewBin(id string, private bool, name string) (*Bin, error) {
+	if strings.TrimSpace(id) == "" {
+		return nil, errors.New("EMPTY_ID")
+	}
+	if strings.TrimSpace(name) == "" {
+		return nil, errors.New("EMPTY_NAME")
+	}
+
+	newBin := &Bin{
+		Id:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
+	}
+
+	return newBin, nil
+}
+
+type BinList struct {
+	bins []*Bin
+}
+
+func NewBinList() *BinList {
+	return &BinList{
+		bins: make([]*Bin, 0),
+	}
+}
