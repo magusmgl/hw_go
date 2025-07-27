b34fa7a79a3371e53b460ea6f86f14809707417d
Maksim G <magus.mgl@gmail.com>
2025-07-27 16:42:57 +0500

    homework json save

--- 3project/file/file.go
+++ 3project/file/file.go
@@ -1,3 +1,31 @@
 package file
 
-func ReadFile() {}
+import (
+	"os"
+
+	"github.com/fatih/color"
+)
+
+func ReadFile(fileName string) ([]byte, error) {
+	data, err := os.ReadFile(fileName)
+	if err != nil {
+		color.Red("Не удалось прочитать файл")
+		return nil, err
+	}
+	return data, nil
+}
+
+func WriteFile(contet []byte, fileName string) {
+	file, err := os.Create(fileName)
+	if err != nil {
+		color.Red("Не удалось создать файл")
+		return
+	}
+	defer file.Close()
+	_, err = file.Write(contet)
+	if err != nil {
+		color.Red("Не удалось записать данные в файл")
+		return
+	}
+	color.Green("Данные записаны в файл")
+}
